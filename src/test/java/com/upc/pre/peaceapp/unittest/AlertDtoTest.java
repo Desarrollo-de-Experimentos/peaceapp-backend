6af08d986a379cf2c5b4edc85d868549b306c009
Arian Rodriguez <arianmrv12@gmail.com>
2025-05-14 20:22:02 -0500

    feat(test): added unit test for alert model

--- src/test/java/com/upc/pre/peaceapp/unittest/AlertDtoTest.java
+++ src/test/java/com/upc/pre/peaceapp/unittest/AlertDtoTest.java
@@ -4,10 +4,11 @@ import com.upc.pre.peaceapp.models.Alert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AlertDtoTest {
     @Test
-    void testValidationFields() {
+    void AlertValidationFields_ValidateFields_AssertEquals() {
         Alert alert = new Alert(
                 "Location",
                 "Type",
@@ -17,9 +18,26 @@ public class AlertDtoTest {
                 1L
         );
 
-        assertEquals("Location2", alert.getLocation(), "La ubicación no coincide con el valor esperado");
+        assertEquals("Location", alert.getLocation(), "La ubicación no coincide con el valor esperado");
         assertEquals("Type", alert.getType(), "El tipo no coincide con el valor esperado");
         assertEquals("Description", alert.getDescription(), "La descripción no coincide");
         assertEquals("ImageUrl", alert.getImageUrl(), "La URL de la imagen no coincide");
+    }
+
+    @Test
+    void SetDescription_DescriptionAlert_ThrowsExceptionMoreThan255() {
+        Alert alert = new Alert(
+                "Location",
+                "Type",
+                "Description",
+                1,
+                "ImageUrl",
+                1L
+        );
+
+        String longDescription = "a".repeat(256); // 256 characters long
+
+        alert.setDescription(longDescription);
+        assertEquals(longDescription, alert.getDescription());
     }
 }
